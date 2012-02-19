251a32145d77c33e77b479816d19321078c33c81
Richard Frith-MacDonald <rfm@gnu.org>
2012-02-19 12:53:41 +0000

    Add copyright
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/devmodules/dev-libs/ec@34778 72102866-910b-0410-8b05-ffd578937521

--- EcBroadcastProxy.h
+++ EcBroadcastProxy.h
@@ -1,14 +1,36 @@
-/* -*-objc-*-
- * Nicola Pero, Brainstorm, October 2000
- * EcBroadcastProxy - an object able to broadcast a message to 
- * a list of remote objects.
- *
- */
+
+/** Enterprise Control Configuration and Logging
+
+   Copyright (C) 2012 Free Software Foundation, Inc.
+
+   Written by: Nicola Pero <nicola.pero@meta-innovation.com>
+   Date: Febrary 2010
+   Originally developed from 1996 to 2012 by Brainstorm, and donated to
+   the FSF.
+
+   This file is part of the GNUstep project.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02111 USA.
+
+   */
 
 #ifndef _ECBROADCASTPROXY_H
 #define _ECBROADCASTPROXY_H
 
-#include <Foundation/NSObject.h>
+#import <Foundation/NSObject.h>
 
 @class	NSArray;
 @class	NSMutableArray;
