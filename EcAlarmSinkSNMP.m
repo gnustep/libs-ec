7d3df9741c1146cbea38b423318aae506f42a288
Richard Frith-MacDonald <rfm@gnu.org>
2015-05-29 15:41:23 +0000

    fixup for using ObjC2 blocks
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/ec/trunk@38586 72102866-910b-0410-8b05-ffd578937521

--- EcAlarmSinkSNMP.m
+++ EcAlarmSinkSNMP.m
@@ -56,6 +56,12 @@ static NSLock		*classLock = nil;
 
 @end
 
+/* The ObjC __block define might conflict with system headers ...
+ *  */
+#if defined(__block)
+#undef __block
+#endif
+
 #include <net-snmp/net-snmp-config.h>
 #include <net-snmp/net-snmp-includes.h>
 #include <net-snmp/agent/net-snmp-agent-includes.h>
